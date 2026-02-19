56b56eaf9ec907df81354ca450e0b6adf8e9b112
jooho-le <wngh0606@naver.com>
2026-02-20 06:46:13 +0900

    init

---
+++ Ch2/inputoutput.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i, j;
+
+    cout << "enter num_1: ";
+    cin >> i;
+
+    cout << "enter num_2: ";
+    cin >> j;
+
+    cout << "num_1 + num_2 = " << i + j << endl;
+    return 0;
+}
