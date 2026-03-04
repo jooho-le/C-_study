09f5252840fcf0ed69eecde70cf609a4829cc290
jooho-le <wngh0606@naver.com>
2026-03-05 01:30:44 +0900

    ch2-5

---
+++ Ch2/ternary.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 7;
+    int b = 5;
+    int result;
+
+    result = a > b ? a : b; // a가 b보다 크면 a를, 그렇지 않으면 b를 result에 저장
+    cout << "result = " << result << endl; 
+
+    return 0;
+}
